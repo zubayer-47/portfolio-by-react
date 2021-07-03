bcf0c95c6ce76cb1e9144e82e563ce5cef717b4a
Zubayer <zubayerabm47@gmail.com>
2021-07-03 18:51:22 +0600

    Fix the bug and re-upload code.

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import Header from './Header/Header'
-import {BrowserRouter, Route} from 'react-router-dom'
-// import Header from './Header/Header'
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header/Header";
+import Intro from './Intro'
+import About from './About'
+import Timeline from './Timeline'
 
 export default function Home() {
   return (
@@ -9,7 +11,9 @@ export default function Home() {
       <div className="grid grid-cols-12">
         <Header />
         <div className=" h-screen bg-red-400 col-span-8 xl:col-span-9">
-          <h1>Hello</h1>
+          <Route exact component={<Intro />} path='/introduction' />
+          <Route component={<About />} path='/about' />
+          <Route component={<Timeline />} path='timeline' />
         </div>
       </div>
     </BrowserRouter>
